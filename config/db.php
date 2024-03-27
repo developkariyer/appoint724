3b15021a4ae7b8cbabfcea05b053c268af9be755
Umut <umut@kariyerfora.com>
2024-03-28 00:28:19 +0300

    gitignore update

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'username' => 'etsy',
+    'password' => 'Gezegen2329',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
