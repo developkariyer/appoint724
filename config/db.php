95707738f1ffca9afc849a06c257a95991da1d01
Umut <umut@kariyerfora.com>
2024-04-05 15:52:21 +0300

    db .env update

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=randevusaas',
-    'username' => 'etsy',
-    'password' => 'Gezegen2329',
+    'dsn' => 'mysql:host='. env('DB_HOST') .';dbname='. env('DB_NAME'),
+    'username' => env('DB_USER'),
+    'password' => env('DB_PASS'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
