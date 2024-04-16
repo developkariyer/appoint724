3384455c0c58c369897b40ff55c0fa62d72cfd54
Umut <umut@kariyerfora.com>
2024-04-16 12:41:35 +0300

    Tidy codebase

---
+++ views/authidentity/_link.php
@@ -0,0 +1,23 @@
+<?php
+/** @var LoginForm $model */
+
+use app\models\LoginForm;
+use yii\widgets\MaskedInput;
+
+$form = yii\bootstrap5\ActiveForm::begin(['id' => 'login-form', 'enableAjaxValidation' => true]);
+
+try {
+    echo $form->field($model, 'emaillink')->widget(MaskedInput::class, [
+        'options' => [
+            'autofocus' => true,
+        ],
+        'clientOptions' => [
+            'alias' => 'email',
+        ],
+    ]);
+} catch (Exception $e) {
+}
+
+echo yii\helpers\Html::submitButton(Yii::t('app', 'Send Link'), ['name' => 'action', 'value' => $model->scenario, 'class' => 'btn btn-primary']);
+
+yii\bootstrap5\ActiveForm::end();
