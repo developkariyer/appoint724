08291252ff6240fcae7616fa292e62a7abea68e0
Umut <umut@kariyerfora.com>
2024-04-22 07:16:51 +0300

    json test

--- controllers/AppointmentController.php
+++ controllers/AppointmentController.php
@@ -17,6 +17,55 @@ use yii\web\NotFoundHttpException;
  */
 class AppointmentController extends Controller
 {
+    
+    public function getEvents()
+    {
+        return [
+            [
+                'title' => 'Morning Meeting #1',
+                'start' => DateTime::createFromFormat('H:i', '03:00'),
+                'duration' => 50,
+                'day' => 0,
+            ],
+            [
+                'title' => 'Morning Meeting #2',
+                'start' => DateTime::createFromFormat('H:i', '05:00'),
+                'duration' => 50,
+                'day' => 1,
+            ],
+            [
+                'title' => 'Morning Meeting #3',
+                'start' => DateTime::createFromFormat('H:i', '07:00'),
+                'duration' => 150,
+                'day' => 2,
+            ],
+            [
+                'title' => 'Morning Meeting #4',
+                'start' => DateTime::createFromFormat('H:i', '09:00'),
+                'duration' => 150,
+                'day' => 0,
+            ],
+            [
+                'title' => 'Morning Meeting #5',
+                'start' => DateTime::createFromFormat('H:i', '10:00'),
+                'duration' => 150,
+                'day' => 0,
+            ],
+            [
+                'title' => 'Morning Meeting #6',
+                'start' => DateTime::createFromFormat('H:i', '13:00'),
+                'duration' => 50,
+                'day' => 0,
+            ],
+            [
+                'title' => 'Lunch Break',
+                'start' => DateTime::createFromFormat('H:i', '15:30'),
+                'duration' => 90,
+                'day' => 0,
+            ],
+        ];
+    }
+
     /**
      * @inheritDoc
      */
@@ -47,51 +96,20 @@ class AppointmentController extends Controller
         return $model;
     }
 
+    public function actionEvents($slug)
+    {
+        return $this->asJson($this->events);
+    }
+
     public function actionView($slug)
     {
-        $events = [
-            [
-                'title' => 'Morning Meeting',
-                'start' => DateTime::createFromFormat('H:i', '03:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Morning Meeting Morning Meeting Morning Meeting Morning Meeting Morning Meeting ',
-                'start' => DateTime::createFromFormat('H:i', '05:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Morning Meeting',
-                'start' => DateTime::createFromFormat('H:i', '07:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Morning Meeting',
-                'start' => DateTime::createFromFormat('H:i', '09:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Morning Meeting',
-                'start' => DateTime::createFromFormat('H:i', '11:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Morning Meeting',
-                'start' => DateTime::createFromFormat('H:i', '13:00'),
-                'duration' => 50,
-            ],
-            [
-                'title' => 'Lunch Break',
-                'start' => DateTime::createFromFormat('H:i', '15:30'),
-                'duration' => 90,
-            ],
-        ];
+        Yii::$app->session->setFlash('info', Yii::t('app', 'Try moving "Morning Meeting" events around :)'));
     
         $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
         $pixPerHour = 40;
 
-        return $this->render('view1', ['events' => $events, 'days' => array_values($days), 'pixPerHour' => $pixPerHour]);
+        return $this->render('view1', ['events' => $this->events, 'days' => array_values($days), 'pixPerHour' => $pixPerHour]);
     }
 
 }
